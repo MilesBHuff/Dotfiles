d51d681a773a35d611551d042b34bea7fdc8a43b
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2024-05-20 16:04:49 -0400

    Better-divvied the graphical display env vars

--- home/miles/.config/plasma-workspace/99-custom.sh
+++ home/miles/.config/plasma-workspace/99-custom.sh
@@ -2,22 +2,10 @@
 ## Copyright (C) by Miles Bradley Huff from 2024 per the LGPL3 (the Third Lesser GNU Public License)
 ## Remember:  This file needs to be compatible with POSIX sh!
 
-## Ensure that apps know what display server to respect
-case "$XGG_SESSION_TYPE" in
-    'wayland') export \
-        GDK_BACKEND=wayland \
-        QT_QPA_PLATFORM=wayland \
-        MOZ_ENABLE_WAYLAND=1 \
-        ;;
-    'x11')
-        ## Do nothing; already set in `~/.xprofile`.
-        ;;
-    *)
-        ## Do nothing; unsupported.
-        ;;
-esac
+## Import display profile.
+FILE="${HOME}/.dprofile" && [ -f "$FILE" ] && . "$FILE"
 
-## Makes some GTK apps use the KDE file dialog.
+## Attempt to make GTK apps use the KDE file dialog.
 export GTK_USE_PORTAL=1
 
 ## Misc
