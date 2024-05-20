3143b601f5d565d6e4561cfe6926d8a4ed495d20
Miles B Huff <MilesBHuff@Users.NoReply.Github.com>
2024-05-20 15:27:17 -0400

    Enhance the environment file for KDE

--- home/miles/.config/plasma-workspace/99-custom.sh
+++ home/miles/.config/plasma-workspace/99-custom.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
-export GDK_BACKEND=wayland
-export QT_QPA_PLATFORM=wayland
-export QT_AUTO_SCREEN_SCALE_FACTOR=1
+
+## Ensure that apps know what display server to respect
+case "$XGG_SESSION_TYPE" in
+    'wayland')
+        export GDK_BACKEND=wayland
+        export QT_QPA_PLATFORM=wayland
+        export MOZ_ENABLE_WAYLAND=1
+        ;;
+    'x11')
+        export GDK_BACKEND=x11
+        export QT_QPA_PLATFORM=xcb
+        export MOZ_ENABLE_WAYLAND=0
+        ;;
+    *)
+        export MOZ_ENABLE_WAYLAND=0
+        ;;
+esac
+
+## Misc
+#export QT_AUTO_SCREEN_SCALE_FACTOR=1
